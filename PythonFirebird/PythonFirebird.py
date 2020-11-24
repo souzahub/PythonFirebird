b4910ebc169a59f88a7bfbfb044156dbbd4d7c71
luansouza88@gmail.com <souzahub>
2020-11-24 13:51:45 -0300

    primeiro commit

--- PythonFirebird/PythonFirebird.py
+++ PythonFirebird/PythonFirebird.py
@@ -1,10 +1,26 @@
 
 import firebirdsql
 
-conn =firebirdsql.connect(user="SYSDBA",password="lmalma",database="c:\\bases\\ibestoqueti\\sgc_ti.gdb", host="170.81.40.199",charset="ANSI")
+usuario= (input("Ditite seu usuario: "))
+senha = (input("Digite sua senha: "))
+caminhoBanco = (input("Digite o caminho do seu Banco de Dados: "))
+ip = (input("Digite o seu Host: "))
+
+
+user = (usuario)
+password = (senha)
+caminhoBd = (caminhoBanco)
+host = (ip)
+
+
+
+conn =firebirdsql.connect(user=(user), password=(password), database=(caminhoBd), host=(host), charset="ANSI ")
+ 
 
 cur=conn.cursor()
-cur.execute("select * from ESTOQUE")
-for c in cur.fetchall():
-    print(c)
+cur.execute("SELECT PRODUTO, ESTOQUE FROM ESTOQUE Where FORNEC = 'CASA DO COMPUTADOR' order by ID;")
+
+for valor in cur.fetchall(): 
+    print(valor)
+
 conn.close
